f21d77021b70062a32b5127d6ca7540a34289815
KyleStulen-1 <62697063+KyleStulen-1@users.noreply.github.com>
2022-04-26 13:26:07 -0500

    PGREEDY

--- main.py
+++ main.py
@@ -53,11 +53,11 @@ def play(world, agent_F, agent_M, policy, max_steps, SARSA=False):
                 action = agent_F.PRANDOM(world.male_current_state[0:2])
                 graph_title = "PRANDOM"
             elif policy == 2:
-                #TODO
                 action = agent_F.PEXPLOIT(world.male_current_state[0:2])
                 graph_title = "PEXPLOIT"
             elif policy == 3:
                 #TODO
+                action = agent_F.PEXPLOIT(world.male_current_state[0:2])
                 graph_title = "PGREEDY"
             reward = world.take_action(action, agent_F.name)
             next_state = world.female_current_state.copy()
@@ -70,11 +70,11 @@ def play(world, agent_F, agent_M, policy, max_steps, SARSA=False):
                 action = agent_M.PRANDOM(world.female_current_state[0:2])
                 graph_title = "PRANDOM"
             elif policy == 2:
-                # TODO
                 action = agent_M.PEXPLOIT(world.female_current_state[0:2])
                 graph_title = "PEXPLOIT"
             elif policy == 3:
                 # TODO
+                action = agent_M.PEXPLOIT(world.female_current_state[0:2])
                 graph_title = "PGREEDY"
             reward = world.take_action(action, agent_M.name)
             next_state = world.male_current_state.copy()
@@ -116,7 +116,7 @@ male_agent = pd_world.Agent("M", world, alpha=0.3, gamma=0.5)
 
 reward_log, steps_at_terminal_log, terminal_y, reward_per_episode_log, F_q_values, M_q_values, title = play(world, female_agent, male_agent, policy=2, max_steps=8000)
 
-run = ["1_b_PEXPLOIT","run_2"]
+run = ["1_c_PGREEDY","run_2"]
 
 # Calculate number of steps between terminal states and store their indexes for graphing
 steps_between_terminal_states = [steps_at_terminal_log[0]]
